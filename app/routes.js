d39cb7820f7b957975efce53b5b24c5291115089
Joyce Ng <joyce.ng@oocl.com>
2025-09-22 11:50:16 +0800

    feat: add ApiClientProvider to App component and fix route casing

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,6 @@ import { index } from "@react-router/dev/routes";
 /**
  * @type {import("@react-router/dev").RouteConfig} RouteConfig
  */
-const routes = [index("routes/home.jsx")];
+const routes = [index("routes/Home.jsx")];
 
 export default routes;
